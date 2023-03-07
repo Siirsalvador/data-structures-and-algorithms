1e1ef34c6362b51af328d1bc1248d7e89f8046ec
Adeola Adebayo <adeola.salvador@gmail.com>
2023-03-07 10:53:46 +0000

    2817_minimum_time_to_complete_trips.py

---
+++ Blind75+Others/2817_minimum_time_to_complete_trips.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        l = 1
+        r = min(time) * totalTrips
+
+        while l < r:
+            m = (l + r) // 2
+
+            if sum(m//t for t in time) >= totalTrips:
+                r = m
+            else:
+                l = m + 1
+        return l
